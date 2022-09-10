55f40ed6b1a8865035afc5dee1e80ec7021651db
iambillmccann <bill.mccann@gmail.com>
2022-09-10 16:37:49 -0400

    Chapter 3. Form

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -16,13 +16,23 @@ const ContactPage = () => {
     <>
       <MetaTags title="Contact" description="Contact page" />
 
-      <Form onSubmit={onSubmit}>
+      <Form onSubmit={onSubmit} config={{ mode: 'onBlur' }}>
         <label htmlFor="name">Name</label>
         <TextField name="name" validation={{ required: true }} />
         <FieldError name="name" className="error" />
 
         <label htmlFor="email">Email</label>
-        <TextField name="email" validation={{ required: true }} />
+        <TextField
+          name="email"
+          validation={{
+            required: true,
+            pattern: {
+              value: /^[^@]+@[^.]+\..+$/,
+              message: 'Please enter a valid email address',
+            },
+          }}
+          errorClassName="error"
+        />
         <FieldError name="email" className="error" />
 
         <label htmlFor="message">Message</label>
